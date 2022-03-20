3c9a0ef50bcc87fd200681d96da0e45f4608afc5
MKesenheimer <mkesenheimer@gmx.net>
2022-03-20 09:28:50 +0100

    Added installl script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod -R 755 Build/Products/Release/CleanAtheros.kext
+sudo chown -R root:wheel Build/Products/Release/CleanAtheros.kext
+sudo kextutil -v Build/Products/Release/CleanAtheros.kext
