5658df746b56284c5c19bcebc5f04ba47c1472e3
MKesenheimer <mkesenheimer@gmx.net>
2022-03-20 09:50:23 +0100

    extended install script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-chmod -R 755 Build/Products/Release/CleanAtheros.kext
-sudo chown -R root:wheel Build/Products/Release/CleanAtheros.kext
-sudo kextutil -v Build/Products/Release/CleanAtheros.kext
+sudo rm -rf CleanAtheros.kext
+sudo cp -R Build/Products/Release/CleanAtheros.kext .
+sudo chmod -R 755 CleanAtheros.kext
+sudo chown -R root:wheel CleanAtheros.kext
+sudo kextutil -v CleanAtheros.kext
 
 # sudo kextcache -i /
